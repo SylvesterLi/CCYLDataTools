6bb8ef36cd9332573b1a9062e66653d7545e7b2e
老李拉面馆 <sangyu.li@outlook.com>
2019-06-04 21:13:32 +0800

    not work

--- webScpTest01.py
+++ webScpTest01.py
@@ -2,13 +2,34 @@ from selenium import webdriver # 从selenium导入webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from datetime import  datetime
+import time
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-# driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=1#feedtop') # 获取百度页面
+print(datetime.now())
+driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=1#feedtop') # 获取百度页面
+try:
+    # driver.get('http://baidu.com')
+    
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(5)
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(2)
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(2)
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    element= WebDriverWait(driver,5,0.5).until(lambda driver:driver.find_element_by_link_text("下一页"))
+    # input = driver.find_element_by_id('kw') #获取输入框
+    
 
-driver.get('http://baidu.com')
-WebDriverWait(driver,120)
-input = driver.find_element_by_id('kw') #获取输入框
+    input=driver.find_element_by_link_text("下一页")
+    input.click()
+
+
+    
+finally:
+    print("Error")
+    print(datetime.now())
 # searchButton = driver.find_element_by_id('su') #获取搜索按钮
 
 # input.send_keys("Python") #输入框输入"Python"
@@ -22,7 +43,6 @@ input = driver.find_element_by_id('kw') #获取输入框
 
 
 
-print(input)
 # 滑到最下面才出现下一页 但是可能还是加载  还要检测下一页是否出现
 
 
