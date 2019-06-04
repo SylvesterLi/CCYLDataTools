01c8706e7adf8eadd94476d2806b905e03e623db
SylvesterLi <lisangyu45@qq.com>
2019-06-04 17:36:28 +0800

    no idea

--- webScpTest01.py
+++ webScpTest01.py
@@ -1,9 +1,14 @@
 from selenium import webdriver # 从selenium导入webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=1#feedtop') # 获取百度页面
+# driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=1#feedtop') # 获取百度页面
 
-# input = driver.find_element_by_id('kw') #获取输入框
+driver.get('http://baidu.com')
+WebDriverWait(driver,120)
+input = driver.find_element_by_id('kw') #获取输入框
 # searchButton = driver.find_element_by_id('su') #获取搜索按钮
 
 # input.send_keys("Python") #输入框输入"Python"
@@ -13,7 +18,11 @@ driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&
 
 
 
+# input=driver.find_element_by_id('Pl_Official_MyProfileFeed__20')
 
+
+
+print(input)
 # 滑到最下面才出现下一页 但是可能还是加载  还要检测下一页是否出现
 
 
