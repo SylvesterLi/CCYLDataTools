61135213d1d24bd0592e4ca06df0552619440f7b
SylvesterLi <lisangyu45@qq.com>
2019-06-04 12:01:06 +0800

    。

--- webScpTest01.py
+++ webScpTest01.py
@@ -1,11 +1,27 @@
 from selenium import webdriver # 从selenium导入webdriver
 
 driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-driver.get('https://www.baidu.com') # 获取百度页面
+driver.get('https://weibo.com/5849524630/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=1#feedtop') # 获取百度页面
 
-input = driver.find_element_by_id('kw') #获取输入框
-searchButton = driver.find_element_by_id('su') #获取搜索按钮
+# input = driver.find_element_by_id('kw') #获取输入框
+# searchButton = driver.find_element_by_id('su') #获取搜索按钮
 
-input.send_keys("Python") #输入框输入"Python"
-searchButton.click() #搜索
+# input.send_keys("Python") #输入框输入"Python"
+# searchButton.click() #搜索
+
+# 等待id为Pl_Official_MyProfileFeed__20的div出现
+
+
+
+
+# 滑到最下面才出现下一页 但是可能还是加载  还要检测下一页是否出现
+
+
+# 这时候再进行检测
+# https://weibo.com/5849524630/profile?pids=Pl_Official_MyProfileFeed__20&is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=4#feedtop
+# 其中page可控制
+# 
+
+# find 阅读xx次
+
 
